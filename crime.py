dc3fbaee385f1406a54b31f2fe3a8f34d776b8bb
Kenia Godinez Nogueda <90576158+gnogueda@users.noreply.github.com>
2022-03-13 01:04:46 -0600

    Second draft. Cleaned data and added filters.

--- crime.py
+++ crime.py
@@ -5,7 +5,6 @@ Dash application
 # This token could be in another file and call it 
 mapbox_access_token = 'pk.eyJ1IjoiZ25vZ3VlZGEiLCJhIjoiY2wwa2Q4ZW1xMGZyaTNlbmVnMDJydHRvcCJ9.d17UBC8JGh_xhi29OHym0w'
 
-# This should be in the virtual environment
 import pandas as pd
 import numpy as np
 import dash
@@ -13,47 +12,123 @@ import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-import plotly.offline as py 
+import plotly.offline as py
 import plotly.graph_objs as go
-import plotly.express as px
 
 df = pd.read_csv("crime_data.csv")
+df['State'] = df['State'].fillna(method = 'ffill') 
+
+# [erase] Alex, NY, for example, is missing from the dataset and this code (ffill)
+# [erase] to fill the values from the first column won't work, I would suggest not to  
+# [erase] repeat variables and getting the dataset as clean as possible before this 
+# [erase] stage
+
+df = df.sort_values('Population', ascending=False).head(25)
+
+df.drop(df.columns.difference(['State', 'City', 'Population', 'Violentcrime',
+       'Murderandnonnegligentmanslaug', 'Rape1', 'Robbery',
+       'Aggravatedassault', 'Propertycrime', 'Burglary', 'Larcenytheft',
+       'Motorvehicletheft', 'Arson2', 'state_id',
+       'lat', 'lng', 'year']), 1, inplace=True)
+
+# Reshape and clean crime names
+df = df.melt(id_vars =['State', 'City', 'Population', 'state_id','lat',
+                       'lng', 'year']) 
+df = df.rename(columns = {"variable": "type_crime", "value": "number_crimes"})
+df = df.replace({'Violentcrime': 'Violent crime',
+            'Murderandnonnegligentmanslaug': 'Murder and nonnegligent manslaughter', 
+            'Rape1': 'Rape',
+            'Aggravatedassault': 'Aggravated assault', 
+            'Propertycrime': 'Property crime',
+            'Larcenytheft': 'Larceny theft',
+            'Motorvehicletheft': 'Motor vehicle theft',
+            'Arson2': 'Arson'})
+blackbold={'color':'black', 'font-weight': 'bold'}
+
+
+# Application layout
 app = dash.Dash(__name__)
-px.set_mapbox_access_token(mapbox_access_token)
+app.layout = html.Div([
 
-# Create map
-fig = px.scatter_mapbox(df, 
-                        lat="lat", 
-                        lon="lon", 
-                        color_continuous_scale=px.colors.cyclical.IceFire, 
-                        size_max=15, 
-                        zoom=10,
-                        hover_name='city',
-                        hover_data=['population', 'Rape1'])
+    html.Div([
+        html.Div([
 
-fig.update_layout(
-    hovermode='closest',
-    mapbox=dict(
-        accesstoken=mapbox_access_token,
-        bearing=0,
-        center=go.layout.mapbox.Center(
-            lat=37,
-            lon=-100
+            # Crimes checklist
+            html.Label(children=['Type of crime: '], style=blackbold),
+            dcc.Checklist(id='crimes',
+                    options=[{'label':str(b),'value':b} for b in sorted(df['type_crime'].unique())],
+                    value=[b for b in sorted(df['type_crime'].unique())],
+            ),
+
+            # Year checklist
+            html.Label(children=['Year: '], style=blackbold),
+            dcc.Checklist(id='year_id',
+                    options=[{'label':str(b),'value':b} for b in sorted(df['year'].unique())],
+                    value=[b for b in sorted(df['year'].unique())],
+            ),
+
+
+        ], className='three columns'
         ),
-        pitch=0,
-        zoom=3
-    )
+
+        # Map
+        html.Div([
+            dcc.Graph(id='graph', config={'displayModeBar': False, 'scrollZoom': True},
+                style={'padding-bottom':'2px','padding-left':'2px','height':'100vh'}
+            )
+        ], className='nine columns'
+        ),
+
+    ], className='row'
+    ),
+
+], className='ten columns offset-by-one'
 )
 
-# Layout of the application
-app.layout = html.Div(children=[
-    html.H1(children='Crime in the major cities of USA, 2019'),
+#---------------------------------------------------------------
+# Output of Graph
+@app.callback(Output('graph', 'figure'),
+              [Input('crimes', 'value'),
+               Input('year_id', 'value')])
 
-    html.Div(children='This dashboard shows the prevalence of crimes...'),
-    html.Div([dcc.Graph(figure=fig, 
-                        style={'padding-bottom':'2px','padding-left':'2px','height':'90vh'})
-              ])
-    ])
+def update_figure(chosen_boro,chosen_recycling):
+    df_sub = df[(df['type_crime'].isin(chosen_boro)) &
+                (df['year'].isin(chosen_recycling))]
+
+    # Create figure
+    locations=[go.Scattermapbox(
+                    lon = df_sub['lng'],
+                    lat = df_sub['lat'],
+                    mode='markers',
+                    unselected={'marker' : {'opacity':1, 'size':10}},
+                    selected={'marker' : {'opacity':0.5, 'size':40}},
+                    hovertext=(df_sub['City'])
+
+    )]
+
+    # Return figure
+    return {
+        'data': locations,
+        'layout': go.Layout(
+            uirevision= 'foo', #preserves state of figure/map after callback activated
+            clickmode= 'event+select',
+            hovermode='closest',
+            hoverdistance=2,
+            title=dict(text="Crime in major cities of USA, 2015-2019",font=dict(size=50, color='black')),
+            mapbox=dict(
+                accesstoken=mapbox_access_token,
+                bearing=0,
+                style='light',
+                center=dict(
+                    lat=37,
+                    lon=-100
+                ),
+                pitch=0,
+                zoom=3
+            ),
+        )
+    }
+
 
 if __name__ == '__main__':
     app.run_server(debug=False)
