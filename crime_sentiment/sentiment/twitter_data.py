cd4b7174b570602482568214adda9471d16d4a17
Rodrigo Salazar <rodrigosalazar@linux5.cs.uchicago.edu>
2022-03-16 16:29:08 -0500

    td last sub

--- crime_sentiment/sentiment/twitter_data.py
+++ crime_sentiment/sentiment/twitter_data.py
@@ -2,6 +2,19 @@
 This file:
     -Scrape tweets
 
+Functions:
+    -tweets_scraping: 
+    -vocabulary_to_search
+    -twint_search_loop
+    -twint_search
+
+Note: The scraping is done by city, vocabulary and day for the next reasons:
+    city: Improve the representation of the tweets from every city despite
+    their large population size differences.
+    vocabulary: Overcome Twitter limitation in the words filtered per search.
+    day: Overcome Twitter attempts to block twint seraches. Performing searches
+    by day allow us to save the data more regularly.
+
 '''
 
 from calendar import month
@@ -14,20 +27,17 @@ import glob
 import shutil
 import time
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+nest_asyncio.apply()
 
-nest_asyncio.apply() # for working in jupyter notebooks
+# Parameters:
+save_dir = os.path.abspath(os.path.join(__file__ ,"../..")) + "/data/twitter/"
+cities = ["new york city", "los angeles", "chicago", "houston", "phoenix",
+"san antonio", "philadelphia", "san diego", "dallas", "austin"]
+# Directory to test this function without affecting the data we used for
+# our analysis:
+dir_for_tweets = "tweets_downloads_test"
 
-# td.tweets_scraping(td.viol_voc)
-cities = ["los angeles", "chicago", "houston", "phoenix", "san antonio", "philadelphia", "san diego", "dallas", "austin"]
-#cities = ["new york city", "los angeles", "chicago", "houston", "phoenix",
-#"san antonio", "philadelphia", "san diego", "dallas", "austin"]
-save_dir = './'
-year = 2022
-monthhh = 1  # lo normal es 1, se refiere al mes en el que empieza
-indices = [1] # lo normal es [0, 1]
-#cities = ["new york city", "los angeles", "chicago", "houston", "phoenix",
-#"san antonio", "philadelphia", "san diego", "dallas", "austin"]
-
+# Vocabularies
 viol_voc_1 = '''violence violent crime shooting gun murder
 assault rape harassment terrorist terrorist
 racist robber arrest supremacist sexist
@@ -46,118 +56,113 @@ weapon knife wound hurt wounded
 offense kidnap kidnapped kidnapping abuse
 aggressive force suicide bully riot'''
 
-# hate terrified terrifying hit die scary scared fear force blood
-
-viol_voc_3 = '''weapon knife wound hurt wounded
-offense kidnap kidnapped kidnapping abuse
-aggressive force suicide bully riot
-protest extremist harm rage threat
-anger retaliate'''
-
 viol_voc = [viol_voc_1, viol_voc_2]
-#viol_voc = [viol_voc_1, viol_voc_2]
+# We kept some words out because they were creating a lot of noise, like:
+# hate terrified hit die scared fear force blood
 
-racism = ['nigger', 'blacky', 'uppity', 'racist', 'racism', 'race', 'racist', 'racism', 'race']
-sexism = ['bitch', 'cunt', 'bimbo', 'feminazi']
-homophobia = ['fag', 'faggot', 'homo'] # another could be like drugs
-
-def go():
-    pass
-
-######################## Tweets scraping section ########################
-'''
-search_term = 'shooting OR killed OR kill OR shot OR gun OR rifle OR pistol OR murder OR murdered 
-OR murderers' OR violence OR violent OR attack OR mug OR mugged OR mugging OR assault OR assaulted 
-OR assaulting OR criminal OR criminals OR harass OR harassment OR sexual OR rape OR rapist OR raped 
-OR raping OR hit OR punch OR punched OR kick OR stab OR weapon OR knife OR wound OR hurt OR injured 
-OR wounded OR offense'  
-'''
-
-'''
-gang band danger dangerous kidnap kidnapped kidnapping robber robbery steal stole stolen hijacking victim
-victims trafficking bomb police fbi death dead died die
-'''
-
-'''
-start_date = dt.datetime(2021, 2, 1)
-end_date = dt.datetime(2021, 2, 3)
-save_dir = './'
-city = ["new york city"]
-td.twint_search_loop(search_term, start_date, end_date, save_dir, city)
-path = save_dir + "tweets_downloads2"
-path
-shutil.rmtree(path)
-'''
-
-def twint_search(search_term, since, until, save_path, city):
-    c = twint.Config()
-    c.Search = search_term
-    c.Lang = "en"
-    c.Near = city #c.New = city
-    c.Limit = 2000000 #10000
-    c.Pandas = True
-    #print(since)
-    #print(since.strftime('%Y-%m-%d %H:%M:%S'))
-    print(since.strftime('%Y-%m-%d'))
-    print(until.strftime('%Y-%m-%d'))
-    c.Since = since.strftime('%Y-%m-%d %H:%M:%S') #"2021-02-01"
-    c.Until = until.strftime('%Y-%m-%d %H:%M:%S') #"2021-02-03"
-    c.Hide_output = True
-    c.Popular_tweets = True
-    c.Filter_retweets = True
-    #c.Min_likes = 100
-    #c.Min_wait_time = 120
-    #c.Min_retweets = 1
-    c.Store_csv = True
-    c.Output = save_path
-    #c.Geo = True
-    twint.run.Search(c)
-    #time.sleep(1)
+def tweets_scraping(vocabulary, day1, month1, year1, day2, month2, year2):
+    '''
+    Scrapes tweets that contain words in the vocabulary during a given period
+    of time.
     
-def twint_search_loop(search_term, start_date, end_date, save_dir, city, ind_voc):
-    # This is the part that keeps it looping even when it errors
+    Inputs:
+        vocabulary(list of strings): List of strings to search in twitter.
+        A list is used to expand the words we can search as twitter limits
+        the number of words to filter every search.
+        day1, month1, year1 (int): day, month and year to begin the search.
+        day2, month2, year2 (int): day, month and year to stop the search.
+
+    Output:
+        CSV files that contain the tweet scraped by city, vocabulary and day.
+
+    '''
+    start_date = dt.datetime(year1, month1, day1)
+    end_date = dt.datetime(year2, month2, day2)
+    for ind_voc, voc in enumerate(vocabulary):
+        search_term = vocabulary_to_search(voc)
+        # Loop through cities
+        for city in cities:
+            twint_search_loop(search_term, start_date, end_date, city, ind_voc)
+
+
+def vocabulary_to_search(vocabulary):
+    '''
+    Prepares the vocabulary to be in the proper format for a twitter
+    search 
+    
+    Inputs:
+        vocabulary(list of strings): List of strings to search in twitter.
+
+    Output:
+        voc_split_or(string): String with the right format.
+
+    '''
+    voc_split = vocabulary.split()
+    voc_split_or = " OR ".join(voc_split)
+    return voc_split_or
+
+
+def twint_search_loop(search_term, start_date, end_date, city, ind_voc):
+    '''
+    Scrapes tweets by date.
+    
+    Inputs:
+        search_term(strings): Words to search in twitter.
+        A list is used to expand the words we can search as twitter limits
+        the number of words to filter every search.
+        start_date (date): Date to begin the search.
+        end_date (date): Date to stop the search.
+        city (string): City where the tweets are scraped
+        ind_voc (ind): Indicates what vocabulary is it using for scraping
+
+    Output:
+        CSV files that contain the tweet scraped by city, vocabulary and day.
+
+    '''
     try:
-        os.makedirs(os.path.join(os.getcwd(),save_dir,"tweets_downloads2"))
+        os.makedirs(os.path.join(os.getcwd(),save_dir, dir_for_tweets))
         print(f'Successfully created the directory {os.path.join(os.getcwd(),save_dir,search_term)}')
     except FileExistsError:
-        print(f'Directory {os.path.join(os.getcwd(),save_dir,"tweets_downloads2")} already exists')
+        print(f'Directory {os.path.join(os.getcwd(),save_dir, dir_for_tweets)} already exists')
     
     date_range = pd.date_range(start_date, end_date)
     
     for single_date in date_range:
         since = single_date
         until = single_date + dt.timedelta(days = 2)
-        save_path = os.path.join(save_dir, "tweets_downloads2", f'{single_date:%Y%m%d}{city}{ind_voc}.csv')
+        save_path = os.path.join(save_dir, dir_for_tweets, f'{single_date:%Y%m%d}{city}{ind_voc}.csv')
         if not os.path.exists(save_path):
         #print(save_path)
             print(f"Searching for tweets containing '{search_term}' from {single_date:%Y-%m-%d} and saving into {save_path}")
             twint_search(search_term, since, until, save_path, city)
 
-def vocabulary_to_search(vocabulary):
-    # Let's keep it in 45 words. We can loop every vocabulary
-    #potential_crime_string = '''shooting killed kill shot gun rifle pistol murder murdered murderers violence violent attack mug mugged mugging assault assaulted assaulting criminal criminals harass harassment sexual rape rapist raped
-    #raping hit punch punched kick stab weapon knife wound hurt injured wounded offense'''
-    #''' felony misdemeanor law abuse force battered'''
-    #potential_crime_string = '''gang band danger dangerous kidnap kidnapped kidnapping robber robbery steal stole stolen hijacking victim
-    #victims trafficking bomb police fbi death dead died die
-    #'''
 
-    voc_split = vocabulary.split()
-    #print(len(pcs_split))
-    voc_split_or = " OR ".join(voc_split)
-    return voc_split_or
+def twint_search(search_term, since, until, save_path, city):
+    '''
+    Search in twitter with twint.
+    
+    Inputs:
+        search_term(strings): Words to search in twitter.
+        since (date): Date to begin the search.
+        until (date): Date to stop the search.
+        save_path (string): path to save the tweets scrapped.
+        city (string): City where the tweets are scraped
 
-# td.tweets_scraping(td.viol_voc)
-def tweets_scraping(vocabulary):
-    #start_date = dt.datetime(2015, 1, 1) # 2021-02-01 # This is feb 1
-    start_date = dt.datetime(year, monthhh, 1)
-    end_date = dt.datetime(year, 3, 14) # This is feb 3
-    save_dir = './'
-    for ind_voc, voc in enumerate(vocabulary):
-        search_term = vocabulary_to_search(voc)
-        # Loop through cities 
-        #cities = ["chicago", "new york city"]
-        for city in cities:
-            if ind_voc in indices: ##### temporal
-                twint_search_loop(search_term, start_date, end_date, save_dir, city, ind_voc)
+    Output:
+        CSV files that contain the tweet scraped by city, vocabulary and day.
+    '''
+    c = twint.Config()
+    c.Search = search_term
+    c.Lang = "en"
+    c.Near = city
+    c.Limit = 2000000
+    c.Pandas = True
+    c.Since = since.strftime('%Y-%m-%d %H:%M:%S')
+    c.Until = until.strftime('%Y-%m-%d %H:%M:%S')
+    c.Hide_output = True
+    c.Popular_tweets = True
+    c.Filter_retweets = True
+    c.Store_csv = True
+    c.Output = save_path
+    twint.run.Search(c)
 
