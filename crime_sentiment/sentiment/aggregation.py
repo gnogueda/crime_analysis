34090990755276205d996518a6851e821c44c72c
sophielogan <91502180+sophielogan@users.noreply.github.com>
2022-03-16 14:16:15 -0500

    Clean & doc string

--- crime_sentiment/sentiment/aggregation.py
+++ crime_sentiment/sentiment/aggregation.py
@@ -1,16 +1,18 @@
-
-# Average Sentiment
 import pandas as pd
 import os
 
-#df = pd.read_csv("tweets_output2021.csv")
-# agg.aggregator("tweets_output2017.csv")
-def aggregator(file):  
+def aggregator(file): 
+    '''
+    Aggregates the twitter cluster and sentiment analysis by city 
+    
+    Input: 
+        file (csv): csv with all the data from twitter, the cluster 
+            and sentiment analysis 
+      
+    Output (csv): csv of aggregates
+    '''
     df = pd.read_csv(file) 
     grouped_df = df.groupby(["near", "Category", "sentiment"])#['jaccard sim score']
     results = grouped_df.count()
     final = results[['Score_cat']]
-    #grouped_df = abbr_frame.groupby(["near", "date"])['sentiment recoded']
-    #results['count sentiment'] = grouped_df.mean()
-    #final.rename({'jaccard sim score': 'mean jaccard sim score'}, axis=1, inplace=True)
     final.to_csv(os.path.join('./', f'results2017.csv'))
