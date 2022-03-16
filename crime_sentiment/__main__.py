8dccf23cf0aee8f6bcd7c701972893bbab0d7d15
Rodrigo Salazar <rodrigosalazar@linux5.cs.uchicago.edu>
2022-03-16 13:15:35 -0500

    in main

--- crime_sentiment/__main__.py
+++ crime_sentiment/__main__.py
@@ -1,4 +1,11 @@
-from crime_sentiment import app
+#from crime_sentiment 
+#import crime_sentiment.app ####maybe not from
+import crime_sentiment.dashboard.crime as crime
 
 if __name__ == '__main__':
-    app.run()
+    #app.run()
+    crime.go_crime()
+    # app.run_server(debug = False)
+
+
+# app.run_server(host = "127.0.0.1", port =888, debug=False)
